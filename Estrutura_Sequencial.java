7f3063c55cc8ba14636b6ba2087c7f0e8301bd20
MattSouza14 <mateussouzabr1@gmail.com>
2024-09-20 17:57:50 -0300

    adição de conteudo estudado

--- Estrutura_Sequencial.java
+++ Estrutura_Sequencial.java
@@ -1,5 +1,6 @@
 // package Java_Learning;
 //import java.util.Locale;
+import java.util.Scanner;
 
 public class Estrutura_Sequencial {
 
@@ -23,6 +24,58 @@ public class Estrutura_Sequencial {
 		  System.out.printf("%.2f\n", altura);//Formatado(por padrão pega o padrão do computador que está usadno
 		  
 		  System.out.printf("Resultado = %f metros", altura);
+
+		    //Casting -> conversão explicita de valores
+			double a;
+			int b;
+			a = 5.0;
+			b = (int) a;//Casting
+			
+			
+			//Entrada de Dados/Leitura
+			//Usar o obj tipo Scanner
+			//import java.util.Scanner;
+			
+			Scanner sc = new Scanner(System.in);
+			
+			String x;
+			x = sc.next();//palavras
+			//sc.next().charAt(0);//Ler um caractere
+			//sc.nextLine();//Lendo texto até a quebra de linha
+			//sc.nextInt();//Numeros inteiros
+			//sc.nextDouble();//Numeros ponto flutuantes
+			System.out.println("Voce digitou: " + x);
+			sc.close();
+			
+			//Algumas funções matematicas 
+			double w = 3.0;
+			double y = 5.0;
+			double z = 4.0;
+			
+			double A,B,C;
+			
+			A = Math.sqrt(w);
+			B = Math.sqrt(y);	
+			C = Math.sqrt(z);
+			
+		   System.out.println(A);
+		   System.out.println(B);
+		   System.out.println(C);
+			
+			A = Math.pow(w, y);
+			B = Math.pow(y, 2.0);	
+			C = Math.pow(5.0, 3.0);
+			
+		   System.out.println(A);
+		   System.out.println(B);
+		   System.out.println(C);
+		   
+		   
+		   A = Math.abs(-4.0);
+		   B = Math.abs(7.6);
+		   
+		   System.out.println(A);
+		   System.out.println(B);
 		  
 		  
 		  

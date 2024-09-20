8e56f26812930095746d3f00c1e2398d52af9e45
MattSouza14 <mateussouzabr1@gmail.com>
2024-09-20 17:56:39 -0300

    Finalização do primeiro modulo e questões basicas

--- Exercicios.java
+++ Exercicios.java
@@ -1,4 +1,4 @@
-// package Java_Learning;
+import java.util.Scanner;
 
 public class Exercicios {
 
@@ -22,8 +22,114 @@ public class Exercicios {
 		System.out.printf("\n Measue with eight decimal places: %f \nRouded(three decimal places): %.3f", 
 				measure, measure);
 		
+		//Faça um programa para ler dois valores 
+		//inteiros, e depois mostrar na tela a soma desses 
+		//números com uma mensagem explicativa, conforme exemplo
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double num1, num2, SOMA;
+		System.out.println("\nDigite o primeiro número: ");
+		num1 = sc.nextDouble();
+		System.out.println("\nDigite o segundo número: ");
+		num2 = sc.nextDouble();
+		
+		SOMA = num1 + num2;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+		//Faça um programa para ler o valor do raio de um círculo, 
+		//e depois mostrar o valor da área deste círculo com quatro
+		//casas decimais conforme exemplos.
+		
+		double raio, area;
+		double PI = 3.14159;
+		
+		raio = sc.nextDouble();
+		
+		area = PI * Math.pow(raio, 2);
+		
+		System.out.printf("A = %.4f", area);
+		
+		//Fazer um programa para ler quatro valores inteiros 
+		//A, B, C e D. A seguir, calcule e mostre a diferença do 
+		//produto de A e B pelo produto de C e D segundo a fórmula: 
+		//DIFERENCA = (A * B - C * D).
+		
+		int A,B,C,D;
+		int dif;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		dif = (A * B - C * D);
+		
+		System.out.printf("Diferença = %d", dif);
+
+		// Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+		// hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+		// decimais
+
+		int numFuncionario;
+		double horasTrabalhadas, valorHoraTrabalhada, salario;
+		
+		numFuncionario = sc.nextInt();
+		horasTrabalhadas = sc.nextDouble();
+		valorHoraTrabalhada = sc.nextDouble();
+		
+		salario = horasTrabalhadas * valorHoraTrabalhada;
+		
+		System.out.printf("NUMBER: %d\nSALARY: %.2f", numFuncionario, salario);
+
+		// Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+		// código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago
+		double cod1, cod2, numPecas1, numPecas2, valor1, valor2, total;
+		
+		cod1 = sc.nextInt();
+		numPecas1 = sc.nextInt();
+		valor1 = sc.nextDouble();
+		cod2 = sc.nextInt();
+		numPecas2 = sc.nextInt();
+		valor2 = sc.nextDouble();
+		
+		total = (valor1 * numPecas1 + valor2 * numPecas2);
+		
+		System.out.printf("VALOR A PAGAR: %.2f", total);
+
+
+		// Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+		// mostre:
+		// a) a área do triângulo retângulo que tem A por base e C por altura.
+		// b) a área do círculo de raio C. (pi = 3.14159)
+		// c) a área do trapézio que tem A e B por bases e C por altura.
+		// d) a área do quadrado que tem lado B.
+		// e) a área do retângulo que tem lados A e B
+
+		double A1, B1, C1, area1, area2, area3, area4, area5;
+		//PI já está declarado em uma questão mais acima
+		
+		A1 = sc.nextDouble();
+		B1 = sc.nextDouble();
+		C1 = sc.nextDouble();
+		
+		area1 = (A1 * C1) / 2;
+		
+		area2 =  PI * Math.pow(C1, 2);
+		
+		area3 = ((A1 + B1) * C1) / 2;
+		
+		area4 = Math.pow(B1, 2);
+		
+		area5 = A1 * B1;
+		
+		System.out.printf("TRIANGULO: %.3f", area1);
+		System.out.printf("\nCIRCULO: %.3f", area2);
+		System.out.printf("\nTRAPEZIO: %.3f", area3);
+		System.out.printf("\nQUADRADO: %.3f", area4);
+		System.out.printf("\nRETANGULO: %.3f", area5);
+
+
 	}
-
 }
-
-

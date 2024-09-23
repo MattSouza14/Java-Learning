cfd57ccd6a90b72fa1cad092a62cefc1f4cd0d71
MattSouza14 <mateussouzabr1@gmail.com>
2024-09-23 19:02:16 -0300

    Adição de conteudos extras(Funções para strings e bitwise) e funções(Sixtaxe e criação de função)

---
+++ src/Introdução JAVA/Funcoes.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Funcoes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int maiorNum = max(a,b,c);
+		
+		System.out.println("O maior número é: " + maiorNum);
+	}
+	
+	public static int max (int x, int y, int z) {
+		int aux;
+		if(x > y && x >z) {
+			aux = x;
+		}
+		else if(y > z) {
+			aux = y;
+		}
+		else {
+			aux = z;
+		}
+		
+		return aux;
+	}
+	
+	
+	
+
+}
---
+++ src/POO/Aula1.java
@@ -0,0 +1,5 @@
+package src.POO;
+
+public class Aula1 {
+    
+}

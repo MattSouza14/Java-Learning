095d42ffcdac46abf25ed0fde3068d4371ab07e6
MattSouza14 <mateussouzabr1@gmail.com>
2024-11-06 18:23:31 -0300

    Exercicios de composição  e substring

---
+++ src/EnumeracoesComposicao.java
@@ -0,0 +1,32 @@
+import entities.Comment;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class EnumeracoesComposicao {
+//Enumerações: tipo especial que serva para especificar de forma literal um conjunto de constantes
+//relacionadas -> enum
+
+//Composição: tipo de associação que permite que um objeto contenha outro
+
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip");
+        Comment c2 = new Comment("Wow that´s awesome");
+
+
+        Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),"Traveling to new Zealand",
+                "I´m to visit this wonderful contry", 12);
+
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+
+        System.out.print(p1);
+    }
+
+}

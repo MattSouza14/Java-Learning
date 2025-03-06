c6e50590881e04fcc84f00a5c38d3882016bd5a1
Mateus Souza <151884795+MattSouza14@users.noreply.github.com>
2025-03-06 09:37:03 -0300

    Update Listas.java

--- src/Listas.java
+++ src/Listas.java
@@ -12,9 +12,17 @@ public class Listas {
     //indexOf(obj), lasIndexOf(obj) - encontrar posição do elemento
     //List<Integer> result = list.stream().filter(x->x >4).collect(Collectors.toList());
         //filtar lista com base em predicado
-    //Integer result = list.stream().filter(x->x>4).findFirst().orElse(null);
+        //Integer result = list.stream().filter(x->x>4).findFirst().orElse(null);
         //encontar primeeira ocorrencia com base em predicado
-
+        // Operações Intermediárias (Transformam o Stream)
+        // filter() → Filtra elementos com base em uma condição.
+        // map() → Transforma elementos em outro formato.
+        // sorted() → Ordena os elementos.
+        //  Operações Terminais (Finalizam o Stream)
+        // collect() → Converte o Stream em lista/conjunto/outro tipo.
+        // findFirst() → Retorna o primeiro elemento correspondente.
+        // count() → Conta os elementos filtrados.
+        // forEach() → Percorre e executa uma ação em cada elemento.
         //Não aceita tipos primitivos
         List<Integer> listInt = new ArrayList<>();
 
